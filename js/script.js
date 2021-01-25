5faca5c539c303b00d55028427c79c0392258203
iantzoulis <ikantzoulis@gmail.com>
2021-01-25 16:57:44 -0500

    Replaced Prev/Next with Return to Top

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-$('.btnNext').click(function() {
-    $('.nav-tabs .active').parent().next('li').find('a').trigger('click');
+$('.btnTop').click(function() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   });
-
-$('.btnPrev').click(function() {
-    $('.nav-tabs .active').parent().prev('li').find('a').trigger('click');
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
